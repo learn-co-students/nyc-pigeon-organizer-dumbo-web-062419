324cee50fbbc00ff100fbae0e027b888359bd6a0
Olga Rosas <olga.rosas@gmail.com>
2019-06-17 15:47:55 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -21,21 +21,49 @@ def nyc_pigeon_organizer(data)
 }
 end
 
-def nyc_pigeon_organizer(data)
-pigeon_list = {}
-data.each do |key, value|
-    value.each do |attributes, pigeons|
-          #binding.pry
-      pigeons.each do |name|
-        #binding.pry
-        if pigeon_list[name]
+def nyc_pigeon_organizer (data)
+  final = {}
 
-        else
-          pigeon_list[name] = {}
-          if pigeons.include?(name)
-            pigeon_list[name][:key] = attributes
-          else
-      end
-    end
-  end
-end
+  data.each do |first_level, all_other|
+    all_other.each do |category, array|
+      array.each do |name|
+        final[name] = {:color => [], :gender => [], :lives => []}
+      end 
+    end 
+  end 
+  x = final.keys
+  data[:color].each do |bird_color, name|
+    name.each do |bird_name|
+      x.each do |item|
+        if bird_name === item
+          final[item][:color] << bird_color.to_s
+        end 
+      end 
+    end 
+  end 
+  data[:gender].each do |gender, type|
+    type.each do |bird_name|
+      x.each do |item|
+        if bird_name === item
+          final[item][:gender] << gender.to_s
+        end 
+      end 
+    end 
+  end 
+  data[:lives].each do |location, name|
+    name.each do |bird_name|
+      x.each do |item|
+        if bird_name === item
+          final[item][:lives] << location
+        end 
+      end 
+    end 
+  end 
+  
+  return final 
+end 
+
+nyc_pigeon_organizer(pigeon_data)
+
+
+
