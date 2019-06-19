7dbd036230c1a9da9a05b17d68288c4269983344
Olga Rosas <olga.rosas@gmail.com>
2019-06-19 16:07:10 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -26,6 +26,7 @@ def nyc_pigeon_organizer (data)
 
   data.each do |first_level, all_other|
     all_other.each do |category, array|
+      binding.pry
       array.each do |name|
         final[name] = {:color => [], :gender => [], :lives => []}
       end
